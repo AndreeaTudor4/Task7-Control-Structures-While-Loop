db604fa4f625fd6f5a0370a9b34d947a8a6e4a98
AndreeaTudor4 <andreeatudor07@gmail.com>
2024-03-02 17:03:03 +0000

    Updated program to use lists and built-in functions

--- while.py
+++ while.py
@@ -1,16 +1,14 @@
-# Initialising the sum and counter with 0
-sum = 0
-counter = 0
+# Initialising a list to store all numbers introduced by the user
+numbers = []
 
 # While block to continuously ask the user for a number, until they enter -1
-# The number is added to the sum of all the number introduced and the counter increases with every input
+# The number is added to the list of all the numbers introduced
 while True:
     try:
         num = float(input("Please insert a number (or -1 to stop): "))
         if num == -1:
             break # Exit the loop if the input is -1
-        counter += 1
-        sum += num
+        numbers.append(num) # Add number to the list
 # Catching non-float inputs
     except ValueError:
         print("""You didn't introduce a valid input.
@@ -18,8 +16,8 @@ Please enter a numerical value.""")
 
 # If block to avoid the dividing by zero error (if the user enters -1 as first number)
 # Calculating the average, rounding it for a more user-friendly output and printing it
-if counter != 0:
-    average = round(float(sum/counter), 2) 
+if numbers:
+    average = round(sum(numbers)/len(numbers), 2) # Using built-in sum and len functions
     print(f"The average of the numbers you entered is: {average}")
 else:
     print("You didn't enter any valid number for calculating the average.")
